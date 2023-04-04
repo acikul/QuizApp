646d1c6a0890f6ebc58492773341bce85fdfcc0d
Luka Ilić <lukailic100@gmail.com>
2023-04-04 14:15:56 +0200

    load questions in questionActivity

--- app/src/main/java/udemy/course/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/udemy/course/quizapp/QuizQuestionActivity.kt
@@ -1,11 +1,16 @@
 package udemy.course.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import udemy.course.quizapp.models.Constants
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        val questionList = Constants.getQuestions()
+        Log.i("QuestionList size: ", "${questionList.size}")
     }
 }
