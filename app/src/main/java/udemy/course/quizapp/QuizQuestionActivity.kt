1779765025ef8f423aa33485edf3599e0fded72b
Luka Ilić <lukailic100@gmail.com>
2023-04-04 15:56:32 +0200

    added loading question from list and filling text and image

--- app/src/main/java/udemy/course/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/udemy/course/quizapp/QuizQuestionActivity.kt
@@ -1,16 +1,53 @@
 package udemy.course.quizapp
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import udemy.course.quizapp.models.Constants
 
 class QuizQuestionActivity : AppCompatActivity() {
+    private var questionText: TextView? = null
+    private var questionImage: ImageView? = null
+
+    private var progressBar: ProgressBar? = null
+    private var progressText: TextView? = null
+
+    private var option1: TextView? = null
+    private var option2: TextView? = null
+    private var option3: TextView? = null
+    private var option4: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
+        questionText = findViewById(R.id.question_text)
+        questionImage = findViewById(R.id.question_image)
+
+        progressBar = findViewById(R.id.progress_bar)
+        progressText = findViewById(R.id.progress_text)
+
+        option1 = findViewById(R.id.option_1)
+        option2 = findViewById(R.id.option_2)
+        option3 = findViewById(R.id.option_3)
+        option4 = findViewById(R.id.option_4)
+
         val questionList = Constants.getQuestions()
-        Log.i("QuestionList size: ", "${questionList.size}")
+        var currentPosition = 1
+        val question = questionList[currentPosition-1]
+
+        questionText?.text = question.questionText
+        questionImage?.setImageResource(question.image)
+
+        progressBar?.progress = currentPosition
+        progressText?.text = "$currentPosition/${progressBar?.max}"
+
+        option1?.text = question.option1
+        option2?.text = question.option2
+        option3?.text = question.option3
+        option4?.text = question.option4
+
     }
 }
